7498ed6e0bf5019d26bda0fe156ebe590b54dcf2
DoctorTone <drtonesoftware@gmail.com>
2021-02-04 17:37:33 +0000

    Added routes and router.

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import Button from "react-bootstrap/Button";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Header from "./components/layout/Header";
 import Intro from "./components/layout/Intro";
 import SearchBar from "./components/layout/SearchBar";
@@ -8,12 +9,18 @@ import "./App.css";
 
 const App = () => {
   return (
-    <div className="App">
-      <Header title="Film Finder" />
-      <Intro />
-      <SearchBar />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Header title="Film Finder" />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+        </Switch>
+        <Intro />
+        <SearchBar />
+        <Footer />
+      </div>
+    </Router>
   );
 };
 
--- client/src/components/layout/Header.js
+++ client/src/components/layout/Header.js
@@ -1,9 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = ({ title, icon }) => {
   return (
     <div className="navbar bg-primary">
       <h1>{title}</h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/browse">Browse</Link>
+        </li>
+      </ul>
     </div>
   );
 };

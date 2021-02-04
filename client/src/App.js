b4affd4406b2a537bdabe7bd1ce61051f737f83c
DoctorTone <drtonesoftware@gmail.com>
2021-02-04 17:30:24 +0000

    Added footer.

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import Button from "react-bootstrap/Button";
 import Header from "./components/layout/Header";
 import Intro from "./components/layout/Intro";
 import SearchBar from "./components/layout/SearchBar";
+import Footer from "./components/layout/Footer";
 
 import "./App.css";
 
@@ -11,6 +12,7 @@ const App = () => {
       <Header title="Film Finder" />
       <Intro />
       <SearchBar />
+      <Footer />
     </div>
   );
 };

d7aa1a998243b365d3874378136bc342bf3886b8
DoctorTone <drtonesoftware@gmail.com>
2021-02-04 23:56:03 +0000

    Add film context provider.

--- client/src/context/films/FilmState.js
+++ client/src/context/films/FilmState.js
@@ -1,3 +1,9 @@
+import React, { useReducer } from "react";
+import { v4 as uuidv4 } from "uuid";
+import FilmContext from "./filmContext";
+import filmReducer from "./filmReducer";
+import { ADD_FILM } from "../types";
+
 const FilmState = (props) => {
   const initialState = {
     films: [
@@ -24,4 +30,25 @@ const FilmState = (props) => {
       },
     ],
   };
+
+  const [state, dispatch] = useReducer(filmReducer, initialState);
+
+  // Add film
+  const addFilm = (film) => {
+    film.id = uuidv4();
+    dispatch({ type: ADD_FILM, payload: film });
+  };
+
+  return (
+    <FilmContext.Provider
+      value={{
+        films: state.films,
+        addFilm,
+      }}
+    >
+      {props.children}
+    </FilmContext.Provider>
+  );
 };
+
+export default FilmState;

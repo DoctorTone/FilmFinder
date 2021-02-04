0eb7c58c48076163f1085d8d236557377a923b20
DoctorTone <drtonesoftware@gmail.com>
2021-02-04 22:59:52 +0000

    Set up film states.

---
+++ client/src/context/films/FilmState.js
@@ -0,0 +1,27 @@
+const FilmState = (props) => {
+  const initialState = {
+    films: [
+      {
+        id: 1,
+        name: "Taste the Blood of Dracula",
+        year: "1972",
+        genre: "vampire",
+        type: "video",
+      },
+      {
+        id: 2,
+        name: "The Exorcist",
+        year: "1973",
+        genre: "possession",
+        type: "blu-ray",
+      },
+      {
+        id: 3,
+        name: "Paranormal Activity",
+        year: "2005",
+        genre: "found footage",
+        type: "blu-ray",
+      },
+    ],
+  };
+};

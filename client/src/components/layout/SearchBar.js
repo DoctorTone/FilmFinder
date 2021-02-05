cba0dad217598dcb73f941ab51b1f7fa47db19d3
DoctorTone <drtonesoftware@gmail.com>
2021-02-05 00:11:41 +0000

    Start to add film search facility.

--- client/src/components/layout/SearchBar.js
+++ client/src/components/layout/SearchBar.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useRef } from "react";
 
 const SearchBar = () => {
+  const text = useRef("");
+
+  const onChange = (e) => {
+    searchFilms(text.current.value);
+  };
+
   return (
     <div>
       <form action="">
         <label htmlFor="search">Find a film</label>
-        <input id="search" type="search" placeholder="Enter film name..." />
+        <input
+          id="search"
+          type="search"
+          placeholder="Enter film name..."
+          ref={text}
+          onChange={onChange}
+        />
       </form>
     </div>
   );

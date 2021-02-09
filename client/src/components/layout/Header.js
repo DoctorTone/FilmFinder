250b4095a8339bfc13c8bab810e48fba20c6a294
DoctorTone <drtonesoftware@gmail.com>
2021-02-09 00:09:47 +0000

    Navbar styling.

--- client/src/components/layout/Header.js
+++ client/src/components/layout/Header.js
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom";
 const Header = ({ title, icon }) => {
   return (
     <div className="bg-primary">
-      <h1>{title}</h1>
-      <ul className="nav">
+      <h1 className="pl-5">{title}</h1>
+      <ul className="nav justify-content-end">
         <li className="nav-item">
           <Link className="nav-link active" to="/">
             Home

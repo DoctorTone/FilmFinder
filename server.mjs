82411a093da95feb6f9cd6935c4e300d357ff79a
DoctorTone <drtonesoftware@gmail.com>
2021-02-18 23:12:27 +0000

    Set up mongodb.

--- server.mjs
+++ server.mjs
@@ -1,8 +1,12 @@
 import express from "express";
+import { router as auth } from "./routes/auth.mjs";
 
 const app = express();
 
 app.get("/", (req, res) => res.json({ msg: "System OK" }));
+
+// Routes
+app.use("/add", auth);
 
 const PORT = process.env.PORT || 5000;
 

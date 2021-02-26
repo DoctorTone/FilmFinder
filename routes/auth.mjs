fd57f8020c757c664c133d087e7a6e329fe3007c
DoctorTone <drtonesoftware@gmail.com>
2021-02-26 01:04:25 +0000

    Start login route.

--- routes/auth.mjs
+++ routes/auth.mjs
@@ -1,7 +1,53 @@
 import express from "express";
-
+import { User } from "../models/User.mjs";
 export const router = express.Router();
+import bcrypt from "bcryptjs";
+import { check, validationResult } from "express-validator";
 
-router.get("/", (req, res) => {
-  res.send("Add films to database");
-});
+router.post(
+  "/",
+  [check("password", "Password is required").exists()],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, password } = req.body;
+
+    try {
+      let user = await User.findOne({ name });
+
+      if (!user) {
+        return res.status(400).json({ msg: "Invalid credentials" });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res.status(400).json({ msg: "Invalid credentials" });
+      }
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        {
+          expiresIn: 360000,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Server error");
+    }
+  }
+);

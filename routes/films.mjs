7b6aea15a1493646d89cb8916a4086bbb5ba0fdf
DoctorTone <drtonesoftware@gmail.com>
2021-02-26 01:47:01 +0000

    Add route for adding a film.

--- routes/films.mjs
+++ routes/films.mjs
@@ -1,9 +1,10 @@
 import express from "express";
 import { Film } from "../models/Films.mjs";
 export const router = express.Router();
+import { auth } from "../middleware/auth.mjs";
 import { check, validationResult } from "express-validator";
 
-// Get logged in user
+// Get all films
 router.get("/", async (req, res) => {
   try {
     const films = await Film.find(req.film.id);
@@ -13,3 +14,32 @@ router.get("/", async (req, res) => {
     res.status(500).send("Server error");
   }
 });
+
+// Add new film
+router.post(
+  "/",
+  [auth, [check("name", "Name is required").not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, year, genre } = req.body;
+
+    try {
+      const newFilm = new Film({
+        name,
+        year,
+        genre,
+      });
+
+      const film = await newFilm.save();
+
+      res.json(film);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);

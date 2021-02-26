58edf5d260543b5fa16a33e27af2da86a6d3b147
DoctorTone <drtonesoftware@gmail.com>
2021-02-26 00:45:02 +0000

    Register a user.

--- routes/users.mjs
+++ routes/users.mjs
@@ -48,9 +48,17 @@ router.post(
         },
       };
 
-      jwt.sign(payload, config.get("jwtSecret"), {
-        expiresIn: 360000,
-      });
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        {
+          expiresIn: 360000,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (error) {
       console.error(err.message);
       res.status(500).send("Server error");

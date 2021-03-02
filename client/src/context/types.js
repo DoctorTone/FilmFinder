36fe0ac3e7fd420e59a458b04f73b4004a63995e
DoctorTone <drtonesoftware@gmail.com>
2021-03-02 23:01:42 +0000

    Add authorisation actions.

--- client/src/context/types.js
+++ client/src/context/types.js
@@ -1,3 +1,12 @@
+// Film actions
 export const ADD_FILM = "ADD_FILM";
 export const SEARCH_FILMS = "SEARCH_FILMS";
 export const CLEAR_SEARCH = "CLEAR_SEARCH";
+
+// Authorisation actions
+export const USER_LOADED = "USER_LOADED";
+export const AUTH_ERROR = "AUTH_ERROR";
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+export const LOGIN_FAIL = "LOGIN_FAIL";
+export const LOGOUT = "LOGOUT";
+export const CLEAR_ERRORS = "CLEAR_ERRORS";

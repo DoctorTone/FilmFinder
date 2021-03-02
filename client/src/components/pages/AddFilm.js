54d0317b410d94a99f648c4b597445a00ed1992f
DoctorTone <drtonesoftware@gmail.com>
2021-03-02 00:34:37 +0000

    Start to format add film form.

--- client/src/components/pages/AddFilm.js
+++ client/src/components/pages/AddFilm.js
@@ -3,7 +3,23 @@ import React from "react";
 const AddFilm = () => {
   return (
     <div>
-      <p>Add Film to database</p>
+      <p>Here you can add a film to the database.</p>
+      <p>You must be a registered user to add films</p>
+      <form action="">
+        <div className="row">
+          <label htmlFor="name" className="col-2 col-form-label">
+            Name:
+          </label>
+          <div className="col-10">
+            <input
+              id="name"
+              type="text"
+              className="form-control"
+              placeholder="film name..."
+            />
+          </div>
+        </div>
+      </form>
     </div>
   );
 };

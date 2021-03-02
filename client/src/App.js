dd7a2a89f11543b7c2cfd290efb18742bc6d2cc3
DoctorTone <drtonesoftware@gmail.com>
2021-03-02 23:25:15 +0000

    Set up auth token.

--- client/src/App.js
+++ client/src/App.js
@@ -6,21 +6,25 @@ import About from "./components/pages/About";
 import AddFilm from "./components/pages/AddFilm";
 
 import FilmState from "./context/films/FilmState";
+import AuthState from "./context/auth/AuthState";
+import setAuthToken from "./utils/setAuthToken";
 
 const App = () => {
   return (
-    <FilmState>
-      <Router>
-        <Header title="Film Finder" />
-        <div className="container">
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/about" component={About} />
-            <Route exact path="/add" component={AddFilm} />
-          </Switch>
-        </div>
-      </Router>
-    </FilmState>
+    <AuthState>
+      <FilmState>
+        <Router>
+          <Header title="Film Finder" />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/about" component={About} />
+              <Route exact path="/add" component={AddFilm} />
+            </Switch>
+          </div>
+        </Router>
+      </FilmState>
+    </AuthState>
   );
 };
 

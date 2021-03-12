962823e187da5e8e7000969002a32e8424c0b008
DoctorTone <drtonesoftware@gmail.com>
2021-03-12 00:40:38 +0000

    Show details and link back to home.

--- client/src/components/films/FilmInfo.js
+++ client/src/components/films/FilmInfo.js
@@ -1,12 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const FilmInfo = ({ location }) => {
   const { data } = location;
 
   return (
-    <div className="text-center">
-      <h2>{data.name}</h2>
-    </div>
+    <>
+      <div className="container text-center">
+        <h2>{data.name}</h2>
+        <p>Starring:</p>
+        <p>Year: {data.year}</p>
+        <p>Genres: {data.genre}</p>
+      </div>
+      <div className="text-right">
+        <Link className="btn btn-primary" to="/">
+          Home
+        </Link>
+      </div>
+    </>
   );
 };
 

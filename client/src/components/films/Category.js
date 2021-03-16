5a9806f419e1a1d8f466fa36d46727610ed773b9
DoctorTone <drtonesoftware@gmail.com>
2021-03-16 01:31:54 +0000

    Format category page.

--- client/src/components/films/Category.js
+++ client/src/components/films/Category.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import FilmContext from "../../context/films/filmContext";
+import FilmItem from "./FilmItem";
 
-const Category = () => {
-  return <div>Info about relevant category.</div>;
+const Category = ({ location: { data: searchText } }) => {
+  const filmContext = useContext(FilmContext);
+
+  const { filterFilmCategory, foundFilms } = filmContext;
+
+  useEffect(() => {
+    filterFilmCategory("year", searchText);
+    // eslint-disable-next-line
+  }, []);
+
+  return (
+    <>
+      <div className="container text-center">
+        <h2 className="mb-5"> Films by : 1970's</h2>
+        {foundFilms.length !== 0 && (
+          <div>
+            <div>
+              {foundFilms.map((film) => (
+                <FilmItem key={film._id} film={film} />
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
+    </>
+  );
 };
 
 export default Category;

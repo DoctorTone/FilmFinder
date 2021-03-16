44d328a851e55fb06909abf144ac4ff5121832ef
DoctorTone <drtonesoftware@gmail.com>
2021-03-16 01:07:36 +0000

    Destructure film parameter.

--- client/src/components/films/FilmInfo.js
+++ client/src/components/films/FilmInfo.js
@@ -1,16 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const FilmInfo = ({ location }) => {
-  const { data } = location;
-
+const FilmInfo = ({ location: { data: film } }) => {
   return (
     <>
       <div className="container text-center">
-        <h2>{data.name}</h2>
+        <h2>{film.name}</h2>
         <p>Starring:</p>
-        <p>Year: {data.year}</p>
-        <p>Genres: {data.genre}</p>
+        <p>Year: {film.year}</p>
+        <p>Genres: {film.genre}</p>
       </div>
       <div className="text-right">
         <Link className="btn btn-primary" to="/">
